d0a7ae4ddf662f15be84bcee28d8bf9ca3e60663
xMatty522 <bronczek522@gmail.com>
2023-03-14 21:57:35 +0100

    initial commit

---
+++ podst js/index.js
@@ -0,0 +1,12 @@
+
+
+const button = document.querySelector('button');
+const kolor = document.querySelector('.kolor');
+
+button.addEventListener('click', () => {
+   
+      kolor.classList.add('active');
+      
+    
+  });
+  
